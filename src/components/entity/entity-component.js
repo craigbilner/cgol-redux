36e9a879a64c4162b28d4f55432c3b86aa1b163b
Craig Bilner <craig.bilner@gmail.com>
2015-10-17 19:35:23 +0100

    Provide a big performance boost by only rendering changed entities

--- src/components/entity/entity-component.js
+++ src/components/entity/entity-component.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import styles from './entity-style';
 
-export default ({toggleValue, id, value, colour}) => (
-  <div
-    onClick={toggleValue.bind(null, {id, curValue: value})}
-    style={styles.comp}
-    >
-    <div style={styles.default}></div>
-    <div style={Object.assign({},styles.alive, {
-      backgroundColor: `#${colour || '2345dc'}`,
-      opacity: value
-    })}></div>
-  </div>
-);
+export default class EntityComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.value !== nextProps.value;
+  }
+
+  render() {
+    const {toggleValue, id, value, colour} = this.props;
+    return (
+      <div
+        onClick={toggleValue.bind(null, {id, curValue: value})}
+        style={styles.comp}
+        >
+        <div style={styles.default}></div>
+        <div style={
+        Object.assign({},styles.alive, {
+            backgroundColor: `#${colour || '2345dc'}`,
+            opacity: value
+        })}></div>
+      </div>
+    );
+  }
+}
