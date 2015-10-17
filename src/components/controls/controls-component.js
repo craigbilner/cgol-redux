efc9528ca552093713836fb470536fde9dd760a5
Craig Bilner <craig.bilner@gmail.com>
2015-10-17 17:37:52 +0100

    Add basic styling

--- src/components/controls/controls-component.js
+++ src/components/controls/controls-component.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import styles from './controls-style';
 
 export default ({toggleInPlay, isInPlay}) => (
-  <div>
-    <div onClick={toggleInPlay}>
+  <div style={styles.comp}>
+    <div style={styles.leftGutter}></div>
+    <div
+      onClick={toggleInPlay}
+      style={
+        Object.assign({},styles.button,{
+          backgroundColor: isInPlay ? '#DE7979' : '#5AB15A'
+        })
+      }>
       {
         isInPlay ? 'pause' : 'play'
       }
     </div>
+    <div style={styles.rightGutter}></div>
   </div>
 );
---
+++ src/components/controls/controls-style.js
@@ -0,0 +1,25 @@
+export default {
+  comp: {
+    display: 'flex',
+    flexFlow: 'row',
+    height: 50,
+    alignItems: 'center'
+  },
+  button: {
+    flex: 3,
+    textAlign: 'center',
+    fontFamily: 'Tahoma',
+    height: 25,
+    color: 'white',
+    padding: '5px 0',
+    cursor: 'pointer'
+  },
+  leftGutter: {
+    flex: 1,
+    height: 25
+  },
+  rightGutter: {
+    flex: 1,
+    height: 25
+  }
+};
