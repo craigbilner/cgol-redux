2731abe2afb9c0c128c90552765db963167989c5
Craig Bilner <craig.bilner@gmail.com>
2015-10-18 00:52:28 +0100

    Fix radium reference

--- src/components/controls/controls-component.js
+++ src/components/controls/controls-component.js
@@ -1,52 +1,68 @@
 import React from 'react';
+import Radium from 'radium';
 import _ from 'lodash';
 import styles from './controls-style';
 
-export default ({toggleInPlay, isInPlay, gameSpeed, toggleGameSpeed}) => (
-  <div style={styles.comp}>
-    <div style={styles.leftGutter}></div>
-    <div
-      onClick={toggleInPlay}
-      style={
+class ControlsComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {toggleInPlay, isInPlay, gameSpeed, toggleGameSpeed} = this.props;
+    return (
+      <div style={styles.comp}>
+        <div style={styles.leftGutter}></div>
+        <div
+          onClick={toggleInPlay}
+          style={
         _.assign({}, styles.gameButton, {
           backgroundColor: isInPlay ? '#DE7979' : '#5AB15A'
         })
       }>
-      {
-        isInPlay ? 'pause' : 'play'
-      }
-    </div>
-    <div style={styles.rightGutter}>
-      <div style={
+          {
+            isInPlay ? 'pause' : 'play'
+          }
+        </div>
+        <div style={styles.rightGutter}>
+          <div style={
           _.assign({}, styles.speedButton, {
             border: `1px solid ${gameSpeed === 1 ? '#4E1212' : '#BFBFBF'}`,
             color: gameSpeed === 1 ? '#4E1212' : '#BFBFBF'
           })
         }
-           onClick={toggleGameSpeed.bind(null, 1)}
-        >
-        1x
-      </div>
-      <div style={
+               onClick={toggleGameSpeed.bind(null, 1)}
+            >
+            1x
+          </div>
+          <div style={
           _.assign({}, styles.speedButton, {
             border: `1px solid ${gameSpeed === 5 ? '#4E1212' : '#BFBFBF'}`,
             color: gameSpeed === 5 ? '#4E1212' : '#BFBFBF'
           })
         }
-           onClick={toggleGameSpeed.bind(null, 5)}
-        >
-        5x
-      </div>
-      <div style={
+               onClick={toggleGameSpeed.bind(null, 5)}
+            >
+            5x
+          </div>
+          <div style={
           _.assign({}, styles.speedButton, {
             border: `1px solid ${gameSpeed === 10 ? '#4E1212' : '#BFBFBF'}`,
             color: gameSpeed === 10 ? '#4E1212' : '#BFBFBF'
           })
         }
-           onClick={toggleGameSpeed.bind(null, 10)}
-        >
-        10x
+               onClick={toggleGameSpeed.bind(null, 10)}
+            >
+            10x
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+ControlsComponent.propTypes = {};
+
+ControlsComponent.defaultProps = {};
+
+export default Radium(ControlsComponent);
